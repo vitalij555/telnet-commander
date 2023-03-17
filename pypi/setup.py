34726252f7b19468c45b7789c90753824d758ca6
Vitalij Gotovskij <vitalij@cdeveloper.eu>
2023-03-17 19:21:09 +0200

    Version 0.0.2

--- pypi/setup.py
+++ pypi/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
   name = 'telnet-commander',            # How you named your package folder (MyLib)
   packages=setuptools.find_packages(),
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Telnet Commander ',   # Give a short description about your library
   long_description=long_description,
